5a62ff8d07e30d24903cf4b1a181ef0278c94771
walker <100868154+wa1ker38552@users.noreply.github.com>
2023-02-15 20:24:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -35,32 +35,37 @@ async def on_message(message):
   Database().set_key('message_processing', time.time())
   if message.author == client.user:
     if message.content.startswith(Database('config.json').load()['prefix']):
+      Database().set_key('message_processing', time.time()-Database().load()['message_processing'])
+      Database().set_key('command_processing', time.time())
       if Database().load()['hidden'] is True:
         reference = copy.copy(message)
         await message.delete()
-      
-      Database().set_key('message_processing', time.time()-Database().load()['message_processing'])
-      Database().set_key('command_processing', time.time())
-      await client.process_commands(reference)
+        await client.process_command(reference)
+      else:
+        await client.process_commands(message)
   else:
-    if not message.channel.id in Database().load()['blacklisted']:
-      db = Database().load()
-      if message.author.id in db['mocking']:
-          async for message in message.channel.history(limit=1): message_content = message.content
-            
-          if 'https://' in message_content: 
-            await message.channel.send(f'`{filter(message_content)}`')
-          elif message.mentions: 
-            await message.channel.send('Naughty mentions')
-          else:
-            await message.channel.send(filter(message_content))
+    try:
+      message.guild.id
+      if message.guild.id in Database().load()['blacklisted']:
+        return
+    except AttributeError: pass
+    db = Database().load()
+    if message.author.id in db['mocking']:
+        async for message in message.channel.history(limit=1): message_content = message.content
+          
+        if 'https://' in message_content: 
+          await message.channel.send(f'`{filter(message_content)}`')
+        elif message.mentions: 
+          await message.channel.send('Naughty mentions')
+        else:
+          await message.channel.send(filter(message_content.replace(db['prefix'], '')))
 
-      if str(message.author.id) in db['reacting']:
-        for reaction in db['reacting'][str(message.author.id)]:
-          await message.add_reaction(reaction)
+    if str(message.author.id) in db['reacting']:
+      for reaction in db['reacting'][str(message.author.id)]:
+        await message.add_reaction(reaction)
 
-      if str(message.author.id) in db['replying']:
-        await message.reply(db['replying'][str(message.author.id)])
+    if str(message.author.id) in db['replying']:
+      await message.reply(db['replying'][str(message.author.id)])
       
 
 reload_cogs()
